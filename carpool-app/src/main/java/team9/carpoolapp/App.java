905ba0e7a9cf8064059c82057f4c3f0c0197a907
Thomas Bahen <tombahen@gmail.com>
2018-09-21 15:00:28 -0400

    added empty maven structure

---
+++ carpool-app/src/main/java/team9/carpoolapp/App.java
@@ -0,0 +1,13 @@
+package team9.carpoolapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
