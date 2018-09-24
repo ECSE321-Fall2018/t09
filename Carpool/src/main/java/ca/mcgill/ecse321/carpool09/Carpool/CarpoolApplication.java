65729b448f85c6f4c2da4292e2b39ab46855aa5f
Julian Armour <35701671+JulianArmour@users.noreply.github.com>
2018-09-24 16:07:12 -0400

    Generated Maven project
    Closes #2

---
+++ Carpool/src/main/java/ca/mcgill/ecse321/carpool09/Carpool/CarpoolApplication.java
@@ -0,0 +1,12 @@
+package ca.mcgill.ecse321.carpool09.Carpool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarpoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarpoolApplication.class, args);
+	}
+}
---
+++ Carpool/src/test/java/ca/mcgill/ecse321/carpool09/Carpool/CarpoolApplicationTests.java
@@ -0,0 +1,16 @@
+package ca.mcgill.ecse321.carpool09.Carpool;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CarpoolApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
