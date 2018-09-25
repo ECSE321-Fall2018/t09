a9459ed4a14787303164b473389c32619c817d29
Julian Armour <julian.armour@videotron.ca>
2018-09-25 14:04:22 -0400

    Refactored project

--- src/main/java/ca/mcgill/ecse321/carpool/CarpoolApplication.java
+++ src/main/java/ca/mcgill/ecse321/carpool/CarpoolApplication.java
@@ -1,4 +1,4 @@
-package ca.mcgill.ecse321.carpool09.Carpool;
+package ca.mcgill.ecse321.carpool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/ca/mcgill/ecse321/carpool/CarpoolApplicationTests.java
+++ src/test/java/ca/mcgill/ecse321/carpool/CarpoolApplicationTests.java
@@ -1,4 +1,4 @@
-package ca.mcgill.ecse321.carpool09.Carpool;
+package ca.mcgill.ecse321.carpool;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
