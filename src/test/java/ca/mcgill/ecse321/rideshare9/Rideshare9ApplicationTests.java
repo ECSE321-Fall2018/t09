f04a642a2dd17a98d12ddb9e4e9cdd5f3cacaff1
Yuxiang (Mark) Ma <35381478+yma67@users.noreply.github.com>
2018-09-27 21:35:06 -0400

    Update Rideshare9ApplicationTests.java

--- src/test/java/ca/mcgill/ecse321/rideshare9/Rideshare9ApplicationTests.java
+++ src/test/java/ca/mcgill/ecse321/rideshare9/Rideshare9ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import static org.junit.Assert.*
+import static org.junit.Assert.*; 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Rideshare9ApplicationTests {
