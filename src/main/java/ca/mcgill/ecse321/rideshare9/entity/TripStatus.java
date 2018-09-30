3e20e55ec90265c0aa1e3d7a71550a36f09cc170
yma67 <yuxiang.ma@mail.mcgill.ca>
2018-09-29 22:41:45 -0400

    Clean
    Co-Authored-By: theREALyudix <martinichauchat@users.noreply.github.com>
    Co-Authored-By: Chris Li <chrisliii@users.noreply.github.com>

--- src/main/java/ca/mcgill/ecse321/rideshare9/entity/TripStatus.java
+++
@@ -1,6 +0,0 @@
-package ca.mcgill.ecse321.rideshare9.entity;
-
-public enum TripStatus {
-REGISTERING, CANCELLED, ON_RIDE, COMPLETE;
-
-}
--- src/main/java/ca/mcgill/ecse321/rideshare9/entity/UserStatus.java
+++
@@ -1,5 +0,0 @@
-package ca.mcgill.ecse321.rideshare9.entity;
-public enum UserStatus {
-ON_RIDE, STANDBY;
-	
-}
