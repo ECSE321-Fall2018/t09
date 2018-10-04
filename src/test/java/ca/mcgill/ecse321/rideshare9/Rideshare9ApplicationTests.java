c0232cd9c968620abce0091647ed24bf88d8eb36
Julian Armour <julian.armour@videotron.ca>
2018-10-04 11:16:45 -0400

    Potentially got tests to work.

--- src/test/java/ca/mcgill/ecse321/rideshare9/Rideshare9ApplicationTests.java
+++ src/test/java/ca/mcgill/ecse321/rideshare9/Rideshare9ApplicationTests.java
@@ -1,15 +1,23 @@
 package ca.mcgill.ecse321.rideshare9;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.when;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.mockito.invocation.InvocationOnMock;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.mockito.stubbing.Answer;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.JacksonTester;
@@ -21,8 +29,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.mockito.ArgumentMatchers.*;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import ca.mcgill.ecse321.rideshare9.controller.StopController;
@@ -33,37 +39,29 @@ import ca.mcgill.ecse321.rideshare9.repository.StopRepository;
 @SpringBootTest
 public class Rideshare9ApplicationTests {
 	private MockMvc mvc;
-	@Mock
-	private StopRepository stopDao;
-	@InjectMocks
-	private StopController stopController;
+	@Mock private Stop stop;
+	@Mock private StopRepository stopDao;
+	@InjectMocks private StopController stopController;
+	
 	private JacksonTester<Stop> jsonStop;
 	
-	@BeforeEach
+	@Before
 	public void setup() {
+		MockitoAnnotations.initMocks(this);
 		JacksonTester.initFields(this, new ObjectMapper());
 		mvc = MockMvcBuilders.standaloneSetup(stopController).build();
 	}
 	
 	@Test
-	public void contextLoads(){
-		assertTrue(true);
-	}
-	
-	@Test
 	public void canCreateStop() throws Exception {
-		Stop s = new Stop();
-		s.setId(1);
-		s.setPrice(1.1f);
-		s.setStopName("test");
-		when(stopDao.createStop(anyString(), anyFloat())).thenReturn(new Stop(1l, 1.1f, "test"));
+		//when(stopDao.createStop(anyString(), anyFloat())).thenReturn(null);
 		Stop s2 = new Stop();
 		s2.setPrice(1.1f);
 		s2.setStopName("test");
 		MvcResult response = mvc.perform(post("/stop/add-stop")
 				.accept(MediaType.APPLICATION_JSON)
-				.contentType(jsonStop.write(s2).getJson())
+				.content(jsonStop.write(s2).getJson())
 				).andReturn();
-		System.out.println(response.getClass().toString());
+		assertNotNull(response);
 	}
 }
