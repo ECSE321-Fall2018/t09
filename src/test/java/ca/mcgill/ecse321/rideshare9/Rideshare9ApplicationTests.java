0264bfd06b4cd1a71ad147f6a7879cf9b48fd160
JulianArmour <35701671+JulianArmour@users.noreply.github.com>
2018-10-03 21:00:12 -0400

    Tests run but getting nullpointerexception

--- src/test/java/ca/mcgill/ecse321/rideshare9/Rideshare9ApplicationTests.java
+++ src/test/java/ca/mcgill/ecse321/rideshare9/Rideshare9ApplicationTests.java
@@ -2,33 +2,68 @@ package ca.mcgill.ecse321.rideshare9;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
+import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.invocation.InvocationOnMock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import ca.mcgill.ecse321.rideshare9.controller.UserController;
-import ca.mcgill.ecse321.rideshare9.entity.User;
-import ca.mcgill.ecse321.rideshare9.entity.UserStatus;
-import ca.mcgill.ecse321.rideshare9.service.UserService;
+import static org.mockito.ArgumentMatchers.*;
 
-@RunWith(SpringRunner.class)
-// @RunWith(MockitoJUnitRunner.class)
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import ca.mcgill.ecse321.rideshare9.controller.StopController;
+import ca.mcgill.ecse321.rideshare9.entity.Stop;
+import ca.mcgill.ecse321.rideshare9.repository.StopRepository;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class Rideshare9ApplicationTests {
+	private MockMvc mvc;
+	@Mock
+	private StopRepository stopDao;
+	@InjectMocks
+	private StopController stopController;
+	private JacksonTester<Stop> jsonStop;
+	
+	@BeforeEach
+	public void setup() {
+		JacksonTester.initFields(this, new ObjectMapper());
+		mvc = MockMvcBuilders.standaloneSetup(stopController).build();
+	}
+	
 	@Test
-	public void mytest() {
+	public void contextLoads(){
 		assertTrue(true);
+	}
+	
+	@Test
+	public void canCreateStop() throws Exception {
+		Stop s = new Stop();
+		s.setId(1);
+		s.setPrice(1.1f);
+		s.setStopName("test");
+		when(stopDao.createStop(anyString(), anyFloat())).thenReturn(new Stop(1l, 1.1f, "test"));
+		Stop s2 = new Stop();
+		s2.setPrice(1.1f);
+		s2.setStopName("test");
+		MvcResult response = mvc.perform(post("/stop/add-stop")
+				.accept(MediaType.APPLICATION_JSON)
+				.contentType(jsonStop.write(s2).getJson())
+				).andReturn();
+		System.out.println(response.getClass().toString());
 	}
 }
