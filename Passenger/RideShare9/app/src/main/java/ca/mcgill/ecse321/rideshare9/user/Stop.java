c78932a1baaa4e5cd852ce714fd36edefdbebb79
Julian Armour <julian.armour@videotron.ca>
2018-10-26 16:53:20 -0400

    Started working on methods to parse an Advertisement JSONObject

---
+++ Passenger/RideShare9/app/src/main/java/ca/mcgill/ecse321/rideshare9/user/Stop.java
@@ -0,0 +1,41 @@
+package ca.mcgill.ecse321.rideshare9.user;
+
+public class Stop {
+    private long id;
+    private String name;
+    private float price;
+
+    public Stop() {
+
+    }
+
+    public Stop(long id, String name, float price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+}
