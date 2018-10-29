52858f736fa82363870f939e874d3fb7c986213a
Julian Armour <julian.armour@videotron.ca>
2018-10-29 16:34:19 -0400

    started UI for AdvertisementViewer

---
+++ Passenger/RideShare9/app/src/main/java/ca/mcgill/ecse321/rideshare9/user/AdvertisementViewerActivity.java
@@ -0,0 +1,15 @@
+package ca.mcgill.ecse321.rideshare9.user;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ca.mcgill.ecse321.rideshare9.R;
+
+public class AdvertisementViewerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advertisement_viewer);
+    }
+}
