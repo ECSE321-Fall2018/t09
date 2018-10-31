3a19ad7b0a541c91315928c1ca210e944537f691
JulianArmour <35701671+JulianArmour@users.noreply.github.com>
2018-10-30 23:18:52 -0400

    Journey should now have status CLOSED when the last seat is filled.

--- src/main/java/ca/mcgill/ecse321/rideshare9/controller/MapperController.java
+++ src/main/java/ca/mcgill/ecse321/rideshare9/controller/MapperController.java
@@ -17,7 +17,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import ca.mcgill.ecse321.rideshare9.entity.Advertisement;
 import ca.mcgill.ecse321.rideshare9.entity.MapperUserAdv;
+import ca.mcgill.ecse321.rideshare9.entity.TripStatus;
 import ca.mcgill.ecse321.rideshare9.entity.User;
 import ca.mcgill.ecse321.rideshare9.entity.helper.MapperBestQuery;
 import ca.mcgill.ecse321.rideshare9.repository.AdvertisementRepository;
@@ -54,7 +56,25 @@ public class MapperController {
     	}
     	User current_user = userv.loadUserByUsername(currentUserName); 
     	if (userv.findUserByUsername(currentUserName) != null) {
-        	return mserv.createMapper(current_user.getId(), advService.findAdv(aid).getId()); 
+    		long userId = current_user.getId();
+    		long advId = advService.findAdv(aid).getId();
+    		Advertisement advertisement = advService.findAdv(advId);
+    		
+    		// if advertisement availale seats > 0 then try to join the trip
+    		if (advertisement.getSeatAvailable() > 0) {
+    			// reduce the number of seats available by 1
+    			advertisement.setSeatAvailable(advertisement.getSeatAvailable() - 1);
+    			
+    			// if seats available is now 0 then change trip status to CLOSED
+    			if (advertisement.getSeatAvailable() == 0) {
+    				advertisement.setStatus(TripStatus.CLOSED);
+    			}
+    			advService.updateAdv(advertisement);
+    			return mserv.createMapper(userId, advId);
+    		}
+    		else {
+				return null;
+			}        	
     	}
     	return null; 
     }
