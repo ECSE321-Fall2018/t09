f9b722342006e45ecb76cb9f5e0d65121e41187d
Julian Armour <julian.armour@videotron.ca>
2018-10-30 21:55:25 -0400

    Added missing import.

--- Passenger/RideShare9/app/src/main/java/ca/mcgill/ecse321/rideshare9/HttpUtils.java
+++ Passenger/RideShare9/app/src/main/java/ca/mcgill/ecse321/rideshare9/HttpUtils.java
@@ -6,6 +6,7 @@ import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
+import cz.msebera.android.httpclient.Header;
 import cz.msebera.android.httpclient.HttpEntity;
 
 public class HttpUtils {
@@ -38,7 +39,7 @@ public class HttpUtils {
         client.get(getAbsoluteUrl(url), params, responseHandler);
     }
 
-    public static void get(Context context, String url, Header[] headers, RequestParams params,AsyncHttpResponseHandler responseHandler){
+    public static void get(Context context, String url, Header[] headers, RequestParams params, AsyncHttpResponseHandler responseHandler) {
         client.get(context,getAbsoluteUrl(url),headers,params,responseHandler);
     }
 
