e410558bbb845c537d912ddcd6455b91d9e04957
MartiniChauchat <a843172479@gmail.com>
2018-11-18 19:19:24 -0500

    update

--- src/main/java/ca/mcgill/ecse321/rideshare9/WebMvcConfig.java
+++ src/main/java/ca/mcgill/ecse321/rideshare9/WebMvcConfig.java
@@ -2,10 +2,10 @@ package ca.mcgill.ecse321.rideshare9;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebMvcConfig extends WebMvcConfigurerAdapter {
+public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
