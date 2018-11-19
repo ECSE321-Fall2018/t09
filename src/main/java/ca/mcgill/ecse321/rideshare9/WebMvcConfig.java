03f20cae3c94d13ef245bef76eb58eade64d54a4
JulianArmour <35701671+JulianArmour@users.noreply.github.com>
2018-11-18 20:53:20 -0500

    Pottential fix to CORS problem

--- src/main/java/ca/mcgill/ecse321/rideshare9/WebMvcConfig.java
+++ src/main/java/ca/mcgill/ecse321/rideshare9/WebMvcConfig.java
@@ -7,11 +7,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
-    			.allowCredentials(false).maxAge(3600).exposedHeaders("Authorization");
-    }
+	 @Override
+	    public void addCorsMappings(CorsRegistry registry) {
+	        registry.addMapping("/**")
+	                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+	    }
 }
